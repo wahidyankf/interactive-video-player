ac4a34f316f21f9e1138cb7149074f7495f9a6f4
Wahidyan Kresna Fridayoka <wahidyankf@gmail.com>
2017-07-27 16:37:54 +0700

    customize mediaelement

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,22 @@
-console.log("Hello from app.js");
+$('video').mediaelementplayer({
+  features: ['playpause',
+    'current',
+    'progress',
+    'duration',
+    'skipback',
+    'speed',
+    'tracks',
+    'quality',
+    'volume',
+    'preview',
+    'fullscreen'
+  ],
+  controlsTimesDefault: 10000,
+  controlsTimeoutMouseLeave: 10000,
+  defaultSpeed: 1.00,
+  skipBackInterval: 10,
+  jumpForwardInterval: 10,
+  speeds: ['2.0', '1.75', '1.50', '1.25', '1.0', '0.75'],
+  speedChar: "x",
+  startLanguage: 'en'
+});
