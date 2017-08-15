5744edc9aba62ce9dc49f1fc3af6a9c3cf585ae1
Wahidyan Kresna Fridayoka <wahidyankf@gmail.com>
2017-08-15 08:34:21 +0700

    formatted with prettier

--- src/js/app.js
+++ src/js/app.js
@@ -1,22 +1,23 @@
 $('video').mediaelementplayer({
-  features: ['playpause',
-    'current',
-    'progress',
-    'duration',
-    'skipback',
-    'speed',
-    'tracks',
-    'quality',
-    'volume',
-    'preview',
-    'fullscreen'
-  ],
-  controlsTimesDefault: 10000,
-  controlsTimeoutMouseLeave: 10000,
-  defaultSpeed: 1.00,
-  skipBackInterval: 10,
-  jumpForwardInterval: 10,
-  speeds: ['2.0', '1.75', '1.50', '1.25', '1.0', '0.75'],
-  speedChar: "x",
-  startLanguage: 'en'
+	features: [
+		'playpause',
+		'current',
+		'progress',
+		'duration',
+		'skipback',
+		'speed',
+		'tracks',
+		'quality',
+		'volume',
+		'preview',
+		'fullscreen'
+	],
+	controlsTimesDefault: 10000,
+	controlsTimeoutMouseLeave: 10000,
+	defaultSpeed: 1.0,
+	skipBackInterval: 10,
+	jumpForwardInterval: 10,
+	speeds: [ '2.0', '1.75', '1.50', '1.25', '1.0', '0.75' ],
+	speedChar: 'x',
+	startLanguage: 'en'
 });
